5ce052608e62a120a63109a64585556e67b343c2
jhengy_work <jhengy@github.com>
2023-10-10 09:09:28 +0800

    Configure redis tcp keepalive via `ClientOptions.socketOptions`

--- src/main/java/com/example/demo/redistimeoutbug/RedisConfig.java
+++ src/main/java/com/example/demo/redistimeoutbug/RedisConfig.java
@@ -2,9 +2,11 @@ package com.example.demo.redistimeoutbug;
 
 import io.lettuce.core.*;
 import org.slf4j.*;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.*;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.*;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -16,8 +18,18 @@ public class RedisConfig {
     public LettuceClientConfigurationBuilderCustomizer tcpKeepAliveReplicaBuilderCustomizer() {
         logger.info("Customize Lettuce configuration Client Options");
 
+        SocketOptions socketOptions = SocketOptions.builder()
+            .keepAlive(SocketOptions.KeepAliveOptions.builder()
+                .enable(true)
+                .idle(Duration.ofSeconds(60))
+                .count(3)
+                .interval(Duration.ofSeconds(60))
+                .build()
+            ).build();
+
         return builder -> builder.clientOptions(
             ClientOptions.builder()
+                .socketOptions(socketOptions)
                 .timeoutOptions(TimeoutOptions.enabled())
                 .build()
         );
